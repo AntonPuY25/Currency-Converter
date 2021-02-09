7276c94c06b6e634770559210427e5aff4ac7f44
AntonPuY25 <puy25@bk.ru>
2021-02-09 14:26:09 +0300

    All commits

---
+++ src/redux/actions.ts
@@ -0,0 +1,45 @@
+import {useDispatch as _useDispatch} from 'react-redux';
+
+export enum ACTIONS_TYPE {
+    CHANGE_CURRENCY_FIELD_TYPE = 'CurrencyExchange/CHANGE_CURRENCY_FIELD_TYPE',
+    CHANGE_CHANGE_ACTION = 'CurrencyExchange/CHANGE_CHANGE_ACTION',
+    CHANGE_CURRENT_CURRENCY = 'CurrencyExchange/CHANGE_CURRENT_CURRENCY',
+}
+
+
+export type ChangeCurrencyFieldType = ReturnType <typeof ChangeCurrencyFieldAC >
+
+export const ChangeCurrencyFieldAC = (amountOfBYN: string, amountOfCurrency: string) => {
+   return {
+       type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE,
+       amountOfBYN,
+       amountOfCurrency
+   } as const
+};
+
+export type ChangeAction = ReturnType<typeof ChangeActionAC>;
+
+
+export const ChangeActionAC = (isBuying: boolean) => {
+    return {
+        type:ACTIONS_TYPE.CHANGE_CHANGE_ACTION,
+        isBuying
+    } as const
+};
+
+export type ChangeCurrentCurrencyType = ReturnType<typeof ChangeCurrentCurrencyAC>;
+
+
+export const ChangeCurrentCurrencyAC = (currentCurrency: string) => {
+    return {
+        type:ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY,
+        currentCurrency
+    } as const
+};
+
+export type CurrencyReducersTypes = ChangeCurrencyFieldType | ChangeAction | ChangeCurrentCurrencyType;
+
+export const useDispatch = ()=>{
+    const dispatch = _useDispatch();
+    return (as:CurrencyReducersTypes)=> dispatch(as)
+}
---
+++ src/redux/selectors/selectors.ts
@@ -0,0 +1,6 @@
+import {IGlobalState} from "../state";
+
+
+
+
+export const getAllSelectors = (state: IGlobalState) => state.currency
